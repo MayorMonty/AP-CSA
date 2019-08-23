c9077323f2fc63ceec51cab5a7f3aa93a36bd699
Brendan McGuire <brendan@bren.app>
2019-08-23 08:58:06 -0400

    Add documentation

--- childrens-songs/OldMacDonald2.java
+++ childrens-songs/OldMacDonald2.java
@@ -1,37 +1,48 @@
 /**
  * This program sings the song Old MacDonald Had a Farm.
  *
- * Version 2 - singIt() method eliminates duplicate code and make it easier to add more animals.
+ * Version 2 - singIt() method eliminates duplicate code and make it easier to
+ * add more animals.
  * 
  * @author Jon Cooper
  * @version August 18, 2016 (v0)
  */
 
+public class OldMacDonald2 {
 
-public class OldMacDonald2 
-{
-    
-    public static void eieio() 
-    { 
+    /**
+     * Sings first and last lines
+     */
+    public static void eieio() {
         System.out.println("Old MacDonald had a farm, E I E I O.");
     }
 
-
-    public static void hadA(String animal) 
-    { 
+    /**
+     * 2nd line
+     * 
+     * @param animal Name of animal on the farm
+     */
+    public static void hadA(String animal) {
         System.out.println("And on that farm he had a " + animal + ", E I E I O.");
     }
 
-
-    public static void withA(String sound) 
-    { 
-        System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
-        System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
+    /**
+     * Penultimate lines, describes the sound that animal makes
+     * 
+     * @param sonund Sound of the animal
+     */
+    public static void withA(String sound) {
+        System.out.println("With a " + sound + " " + sound + " here and a " + sound + " " + sound + " there.");
+        System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");
     }
 
-    
-    public static void singIt(String animal, String sound) 
-    {
+    /**
+     * Sings a whole version for a given animal and sound
+     * 
+     * @param animal Animal to sing about
+     * @param sound  That animal's sound
+     */
+    public static void singIt(String animal, String sound) {
         eieio();
         hadA(animal);
         withA(sound);
@@ -39,20 +50,17 @@ public class OldMacDonald2
         System.out.println();
     }
 
-    
-    public static void main(String[] args) 
-    {
-        System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
+    public static void main(String[] args) {
+        System.out.println("Old MacDonald Had A Farm\n"); // Give our song a title
 
         singIt("duck", "quack");
         singIt("cow", "moo");
         singIt("horse", "neigh");
-        singIt("platypus","gdggdggddgdg");
-        
-        // now see how easy it is to add some more animals  
-        
-        
+        singIt("platypus", "gdggdggddgdg");
+
+        // now see how easy it is to add some more animals
+
         // modify the methods above to get quotation marks around the animal sounds
     }
-    
+
 }
