32998e432b836d0f4df1388cf49f71e3970ff603
Brendan McGuire <brendan@bren.app>
2019-12-03 09:24:02 -0500

    Add prompt and close scanner

--- MagpieActivityStarterCode/activity3/MagpieRunner3.java
+++ MagpieActivityStarterCode/activity3/MagpieRunner3.java
@@ -2,28 +2,30 @@ import java.util.Scanner;
 
 /**
  * A simple class to run the Magpie class.
+ * 
  * @author Laurie White
  * @version April 2012
  */
-public class MagpieRunner3
-{
+public class MagpieRunner3 {
 
 	/**
 	 * Create a Magpie, give it user input, and print its replies.
 	 */
 	public static void main(String[] args)
 	{
-		Magpie3 maggie = new Magpie3();
-		
-		System.out.println (maggie.getGreeting());
-		Scanner in = new Scanner (System.in);
+		Magpie2 maggie = new Magpie2();
+
+		System.out.println(maggie.getGreeting());
+		Scanner in = new Scanner(System.in);
+		System.out.print("> ");
 		String statement = in.nextLine();
-		
-		while (!statement.equals("Bye"))
-		{
-			System.out.println (maggie.getResponse(statement));
+
+		while (!statement.equals("Bye")) {
+			System.out.println(maggie.getResponse(statement));
+			System.out.print("> ");
 			statement = in.nextLine();
 		}
-	}
+
+		in.close();
 
 }
