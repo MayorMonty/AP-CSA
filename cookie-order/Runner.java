cdac73c755db90e1c73bc261c8210f84ed90a133
Brendan McGuire <brendan@bren.app>
2020-02-04 09:33:29 -0500

    Runner

---
+++ cookie-order/Runner.java
@@ -0,0 +1,20 @@
+public class Runner {
+
+    public static void main(String argv[]) {
+
+        MasterOrder orders = new MasterOrder();
+
+        orders.addOrder(new CookieOrder("Chocolate Chip", 10));
+        orders.addOrder(new CookieOrder("Brownie", 16));
+        orders.addOrder(new CookieOrder("Shortbread", 14));
+        orders.addOrder(new CookieOrder("Walnut", 12));
+
+        System.out.println("Inital Contents:");
+        orders.showOrder();
+
+        orders.removeVariety("Chocolate Chip");
+        System.out.println("'Chocolate Chip' Removed:");
+        orders.showOrder();
+    }
+
+}
